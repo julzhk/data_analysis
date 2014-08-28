2b197249dbc99c3ce2e1d24ec0f9d66d46b35405
Julian Harley <julian.harley@gmail.com>
2014-08-28 23:25:47 +0100

    add some more functions & tests

--- chapter2code/1_usgovbitly.py
+++ chapter2code/1_usgovbitly.py
@@ -4,7 +4,7 @@ import pprint
 import psutil
 import unittest
 from collections import Counter
-
+from pandas import DataFrame, Series
 def memory_usage():
     # return the memory usage in MB
     # Why we don't use non-lazy iterables? Memory consumption!
@@ -30,6 +30,18 @@ def counting_time_zones(records):
                   and 'tz' in rec]
     return dict(Counter(time_zones))
 
+def top_counts(count_dict, n=10):
+    counts = Counter(count_dict)
+    return counts.most_common()
+
+def top_counts_simple(count_dict, n=10):
+    """
+    These two top_counts are equivalent; this is a bit neater though
+    """
+    value_key_pairs = [(tz, count) for tz, count in count_dict.items()]
+    value_key_pairs.sort(reverse=True)
+    return value_key_pairs
+
 class TestUSGovData(unittest.TestCase):
     def test_naive_extract_first_row(self):
         results = naive_extract_rows()
@@ -41,6 +53,15 @@ class TestUSGovData(unittest.TestCase):
         counts = counting_time_zones(records)
         self.assertTrue(counts['America/Los_Angeles'] == 14)
         self.assertTrue(counts['Europe/Rome'] == 3)
+
+    def test_top_counts(self):
+        records = naive_extract_rows()
+        counts = counting_time_zones(records)
+        toplist = top_counts(counts)
+        toplist_simple = top_counts_simple(counts)
+        self.assertEqual (toplist[0][1] , 25)
+        self.assertEqual(toplist[2][1] , 22)
+
 
 if __name__ == '__main__':
     unittest.main()
